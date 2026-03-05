3483e949e7e4bfe8bfa78dde9372c84a1730e231
Christian Amanuel <christianamanuel64@gmail.com>
2026-03-05 18:13:38 +0100

    fix formatting issues

--- app/types/bruker.ts
+++ app/types/bruker.ts
@@ -1,12 +1,8 @@
 import type {PersonCore,ContactInfo} from "@/types/person"
-
 export interface  User extends PersonCore,ContactInfo{
     tlf: string,
     period: string;
     study: string;
     place: string;
     active: string;
-    
-    
-
 }
