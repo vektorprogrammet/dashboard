ab1ead8d7198413d62d410529d46752cbf5f983b
Christian Amanuel <christianamanuel64@gmail.com>
2026-03-09 17:33:11 +0100

    fix biome issues

--- app/types/person.ts
+++ app/types/person.ts
@@ -1,11 +1,11 @@
-export interface PersonCore{
-    firstName: string;
-    lastName: string;
+export interface PersonCore {
+  firstName: string;
+  lastName: string;
 }
-export interface ContactInfo{
-    mail: string;
-    tlf?: string;
+export interface ContactInfo {
+  mail: string;
+  tlf?: string;
 }
-export interface HasSchool{
-    school: string;
+export interface HasSchool {
+  school: string;
 }
--- app/types/soker.ts
+++ app/types/soker.ts
@@ -1,6 +1,6 @@
-import type { PersonCore, ContactInfo } from "@/types/person";
+import type { ContactInfo, PersonCore } from "@/types/person";
 export interface Soker extends PersonCore, ContactInfo {
-    study: string;
-    lang: string;
-    status: string;
+  study: string;
+  lang: string;
+  status: string;
 }
