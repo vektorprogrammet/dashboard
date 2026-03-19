02751f221bd6fc3f5201c1861073b021998dc849
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-19 18:52:35 +0100

    added query to get all unhandled expenses

--- app/routes/dashboard.utlegg._index.tsx
+++ app/routes/dashboard.utlegg._index.tsx
@@ -1,4 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+
 // biome-ignore lint/style/noDefaultExport: Route Modules require default export https://reactrouter.com/start/framework/route-module
+const tempSdk: any = {};
+
+const { data: expenses } = useQuery({
+  queryKey: ["expenses"],
+  queryFn: () => tempSdk.expenses.listUnhandled()
+})
+
+console.log(expenses);
+
 export default function Utlegg() {
   return (
     <>
