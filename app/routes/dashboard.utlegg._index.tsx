bb165be1d0bca9562078f745a3295001ba19fd37
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-19 18:53:38 +0100

    changed name of data to unhandled

--- app/routes/dashboard.utlegg._index.tsx
+++ app/routes/dashboard.utlegg._index.tsx
@@ -3,12 +3,10 @@ import { useQuery } from "@tanstack/react-query";
 // biome-ignore lint/style/noDefaultExport: Route Modules require default export https://reactrouter.com/start/framework/route-module
 const tempSdk: any = {};
 
-const { data: expenses } = useQuery({
+const { data: unhandled } = useQuery({
   queryKey: ["expenses"],
   queryFn: () => tempSdk.expenses.listUnhandled()
 })
-
-console.log(expenses);
 
 export default function Utlegg() {
   return (
