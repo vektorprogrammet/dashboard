70c7efb6f2349167cc3f386f531a11333d3946fe
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-23 17:33:21 +0100

    added comment describing functionality of query

--- app/routes/dashboard.utlegg._index.tsx
+++ app/routes/dashboard.utlegg._index.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 // biome-ignore lint/style/noDefaultExport: Route Modules require default export https://reactrouter.com/start/framework/route-module
 const tempSdk: any = {};
 
+// Should be used to display all unhandled expenses
 const { data: unhandled } = useQuery({
   queryKey: ["expenses"],
   queryFn: () => tempSdk.expenses.listUnhandled()
