2c5a8ab57d8fa35047c6694b747f8c824e7dceb1
Tobias Hegge <tobiaheg@stud.ntnu.no>
2026-03-23 18:30:49 +0100

    added query to list all sponsors

--- app/routes/dashboard.sponsorer._index.tsx
+++ app/routes/dashboard.sponsorer._index.tsx
@@ -1,4 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+
 // biome-ignore lint/style/noDefaultExport: Route Modules require default export https://reactrouter.com/start/framework/route-module
+const tempSdk: any = {};
+
+// Should be used to display all sponsors
+const { data: sponsors } = useQuery({
+  queryKey: ["sponsors"],
+  queryFn: () => tempSdk.sponsors.list()
+})
+
 export default function Sponsorer() {
   return (
     <>
